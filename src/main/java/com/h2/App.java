a4356e6ab9004ded63d0a3c5d08a8b9353bdce4b
quatreG <georgearwechu@gmail.com>
2020-10-07 14:49:13 +0300

    Completed module

--- src/main/java/com/h2/App.java
+++ src/main/java/com/h2/App.java
@@ -19,7 +19,7 @@ public class App
     private static int add (int[] numbers){
         int sum=0;
         for(int i=1; i<=numbers.length; i++){
-            sum+=1;
+            sum+=i;
 
         }
      return  sum;
