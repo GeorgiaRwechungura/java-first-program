1dfca08ace9d72659df650c1dff2fe501b8fa829
quatreG <georgearwechu@gmail.com>
2020-10-07 15:23:01 +0300

    Completed module

---
+++ src/main/java/com/h2/SavingsCalculator.java
@@ -0,0 +1,20 @@
+package com.h2;
+
+public class SavingsCalculator {
+    private float[] credits;
+    private float[] debits;
+
+    public SavingsCalculator(float[] credits, float[] debits){
+        this.credits=credits;
+        this.debits=debits;
+
+    }
+    private float sumOfCredits(){
+        float sum=0.0f;
+        for(int i=1; i<=credits.length; i++){
+            sum+=i;
+        }
+        return sum;
+
+    }
+}
